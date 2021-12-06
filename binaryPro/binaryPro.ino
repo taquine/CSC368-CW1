c80e8ee1f0f3eda28db031365b48db93ab446526
rosenfoi <karim.nouda@gmail.com>
2021-12-06 17:45:45 +0000

    fixed previous commits

--- binaryPro/binaryPro.ino
+++ binaryPro/binaryPro.ino
@@ -5,16 +5,18 @@
     Karim Stefan Nouda - 1914729
 
 */
-typedef uint16_t fixed_point_t;
 
 #include "dotDevice.h"
 #include "OneWire.h"
 #include "DallasTemperature.h"
 
+#include "driver/adc.h"
+#include <esp_wifi.h>
+#include <esp_bt.h>
+
 #define mS_TO_S_FACTOR 1000  // Conversion factor for milli-seconds to seconds
 #define FIXED_POINT_FRACTIONAL_BITS 8 //amount of bits dedicated to the fractional section
 #define TEMPERATURE_PRECISION 9 //x bit data reading
-//#define ASYNC_DELAY 188 //10 bit requires 187 ms for conversion
 
 OneWire oneWire(26); //set up the oneWire protocol on pin 26
 DallasTemperature sensors(&oneWire);
@@ -27,8 +29,6 @@ const char* gid = "Nx4gVDM1";
 
 dotDevice s_con(ssid, password, server); //connection object
 
-float tempsForAvg[16]; // list of recent temps to be used for the average
-
 struct reading
 {
     uint16_t time;
@@ -37,7 +37,7 @@ struct reading
 
 #pragma pack(1)
 struct tempPayload { //total of 76 bytes
-    char gid[8] = {'N','x','4','g','V','D','M','1'}; //8 bytes (1 per char)
+    char gid[8] = {'N','x','4','g','V','D','M','1'}; //8 bytes (1 per char) 
     uint16_t cmd = 1; //2 bytes
     uint16_t avg; //2 bytes
     struct reading values[16]; // 16 * (2 bytes for time + 2 bytes for fixed point temp) even:time odd:temp
@@ -52,11 +52,12 @@ uint16_t floating_to_fixed(float inp) {
 
 void getBINpayload() {
     float sum = 0.0;
+    float tmptemp;
     for (int i = 0; i <16; ++i){
         data_packet.values[i].time = (uint16_t)millis();
-        tempsForAvg[i] = getTemp();
-        data_packet.values[i].temp = floating_to_fixed(tempsForAvg[i]);
-        sum += tempsForAvg[i];
+        tmptemp = getTemp();
+        data_packet.values[i].temp = floating_to_fixed(tmptemp);
+        sum += tmptemp;
     }
     data_packet.avg = floating_to_fixed(sum/16.0);
     return;
@@ -68,22 +69,28 @@ float getTemp() {
 }
 
 void setup() {
+    adc_power_off();
+    
+    btStop();
+    esp_bt_controller_disable();
+    
     sensors.setResolution(TEMPERATURE_PRECISION);
-    setCpuFrequencyMhz(80);
-    //sensors.setWaitForConversion(false);
-    //esp_sleep_enable_timer_wakeup(ASYNC_DELAY * mS_TO_S_FACTOR);
+    setCpuFrequencyMhz(90);
+    
+
 }
 
 void loop() {
     getBINpayload();
 
-    //setCpuFrequencyMhz(120);
-
     s_con.connect();
     s_con.sendBIN((char*)&data_packet,sizeof(data_packet));
     delay(8);
 
-    esp_sleep_enable_timer_wakeup((30000 - millis()) * mS_TO_S_FACTOR); //no idea why this doesnt get a consistent result
+    esp_wifi_stop();
+
+
+    esp_sleep_enable_timer_wakeup((30000 - millis()) * mS_TO_S_FACTOR);
     esp_deep_sleep_start();
 
 }
